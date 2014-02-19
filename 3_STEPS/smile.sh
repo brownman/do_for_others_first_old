e3ab0552ebfc97f9c9c7a7c128eac02e38f247d7
brownman <brownman556@gmail.com>
2014-02-19 11:32:45 +0000

    3_STEPS: update: motivation

--- 3_STEPS/smile.sh
+++ 3_STEPS/smile.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+pushd `dirname $0`> /dev/null
+dir=`pwd`
 clear
+
+
+edit(){
+eval $EDITOR_GUI $file
+}
+
+
+run(){
 echo
 
 echo "INCREASE YOUR EFFICIENCY!"
@@ -7,8 +17,17 @@ echo "how?"
 echo "wait.. 5 seconds and breath"
 sleep 5
 dir=`pwd`
+file=$dir/priorities.yml
 #dir_link=$dir/links
 #$dir_link/wrapper.sh
-
-
-cat priorities.yml
+cat $file
+echo
+echo
+echo 'press control+c : for updating your priorities'
+echo
+echo 'sleep 10 seconds..'
+trap edit SIGINT
+sleep 10
+}
+run
+popd
