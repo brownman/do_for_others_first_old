fd5f719cc5de7fb05ed710611fc6a8d79e1bff4c
brownman <brownman556@gmail.com>
2014-02-19 14:54:08 +0000

    croning: update

--- PACKAGE/usr/src/plugins/croning/croning.sh
+++ PACKAGE/usr/src/plugins/croning/croning.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash 
 clear
 echo 'this script must be independent'
 sleep 5
@@ -9,46 +9,53 @@ SUDO=''
 if [ "$(id -u)" != "0" ]; then
     user='user'
     SUDO=''
+    set -e
 else
     user='root'
     SUDO='sudo'
 fi
+
+/usr/bin/flite -t "$user"
 user_name=${NAME:-$LOGNAME}
-
 #/usr/bin/notify-send 'user name:' "$user_name"
-/usr/bin/notify-send 'user name:' "$user_name"
-
-
+/usr/bin/notify-send "type: $user" "name: $user_name"
 run(){
-   echo run
+    echo run
     plugin_name="$1"
     time1=`date | cut -d' ' -f4 | sed 's/:/_/g'`
-    dir="/home/$user_name/Desktop/croning"
-    if [ ! -d "$dir" ];then
-        echo "creating directory: $dir"
-        mkdir "$dir"
-        chmod 1777 $dir
-    fi
-    
+    path="/home/$user_name"
 
-    file=$dir/${time1}_${user}.txt
-    touch $file
-    crontab -l > $file
-    flite -t "$user"
-    util=/usr/games/xcowsay
-    $util "$user"
-    if [ "$plugin_name" != '' ];then
-        notify-send 'plugin:' "$plugin_name" 
-        file="$dir_plugins/$plugin_name/wrapper.sh"
-        if [ -f "$file" ];then
-       $file  
-        else
-        echo reason_of_death 'no such file' "$file"
+    if [ -d "$path" ];then
+        dir="$path/Desktop/croning"
+        if [ ! -d "$dir" ];then
+            echo "creating directory: $dir"
+            mkdir "$dir"
+            chmod 1777 $dir
         fi
-        
+        file=$dir/${time1}_${user}.txt
+        touch $file
+        crontab -l > $file
 
+        util=/usr/games/xcowsay
+        $util "$user"
+        if [ "$plugin_name" != '' ];then
+            notify-send 'plugin:' "$plugin_name" 
+            dir="$dir_plugins/$plugin_name"
+            if [ -d "$dir" ];then
+                file=$dir/wrapper.sh 
+                if [ -f "$file" ];then
+                    $file  
+                else
+                    echo reason_of_death 'no such file' "$file"
+                fi
+            else
+                echo reason_of_death 'no such dir' "$dir"
+            fi
+        else
+            echo reason_of_death 'supply a plugin name'
+        fi
     else
-        echo reason_of_death 'no plugin name '
+        echo reason_of_death 'no such path' "$path"
     fi
 }
 
@@ -56,14 +63,13 @@ run(){
 install(){
     echo install
     echo -e "\t\t\t\tRESULTS:"
-echo    print_color 35 "user: $user"
-        file=$dir/txt/crontab.$user.txt
-        $SUDO crontab $file
-        $SUDO crontab -l
-   }
-   
-arg=${1:-''}
+    echo    print_color 35 "user: $user"
+    file=$dir/txt/crontab.$user.txt
+    $SUDO crontab $file
+    $SUDO crontab -l
+}
 
+arg=${1:-''}
 if [ "$arg" = install ];then
     install
 else
