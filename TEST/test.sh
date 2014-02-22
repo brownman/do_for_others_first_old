f1cc27b789a4b41cf91f40322a17bbc87cc7ecaa
brownman <brownman556@gmail.com>
2014-02-22 15:41:33 +0000

    .travis - pass! please!

--- TEST/test.sh
+++ TEST/test.sh
@@ -2,12 +2,14 @@
 #dir=package/do-for-others-first-0.0.1/usr/local/src/do-for-others-first/
 #wrapper=$dir/breath.sh
 ###########################################  Directory structure:  ######################\
+pushd `dirname $0`
 unset rm
 dir=`pwd`
 dir_test=$dir/tests
 export runner=$dir/lib/shunit2/src/shell/shunit2
 $dir_test/about_grep.sh
 $dir_test/about_sed.sh
+popd
 
 
 
