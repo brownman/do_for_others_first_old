0054cdbcc43bd12f1c449b5d8d8f5ccec87af286
brownman <brownman556@gmail.com>
2014-03-03 18:17:44 +0000

    dir_struct: add folder:interview - where new mindmap and lectures text should be located

--- works/root/0.0.1/rc/lib/tmp/commands.sh
+++ works/root/0.0.1/rc/lib/tmp/commands.sh
@@ -109,3 +109,15 @@ cfg.public.history.save
 source /home/dao04/JAIL/TREE/works/root/0.0.1/rc/lib/cfg/public/history/save.cfg
 cfg.public.prompt.history_sync
 source /home/dao04/JAIL/TREE/works/root/0.0.1/rc/lib/cfg/public/prompt/history_sync.cfg
+cfg.public.color.color
+source /home/dao04/JAIL/TREE/works/root/0.0.1/rc/lib/cfg/public/color/color.cfg
+cfg.public.history.save
+source /home/dao04/JAIL/TREE/works/root/0.0.1/rc/lib/cfg/public/history/save.cfg
+cfg.public.prompt.history_sync
+source /home/dao04/JAIL/TREE/works/root/0.0.1/rc/lib/cfg/public/prompt/history_sync.cfg
+cfg.public.color.color
+source /home/dao04/JAIL/TREE/works/root/0.0.1/rc/lib/cfg/public/color/color.cfg
+cfg.public.history.save
+source /home/dao04/JAIL/TREE/works/root/0.0.1/rc/lib/cfg/public/history/save.cfg
+cfg.public.prompt.history_sync
+source /home/dao04/JAIL/TREE/works/root/0.0.1/rc/lib/cfg/public/prompt/history_sync.cfg
