98a6189749eb91e30ca3f37af64817a29772c47f
brownman <brownman556@gmail.com>
2014-03-11 16:38:34 +0000

    breath.sh - generate dymanicly: alias cdX=cd_dir_X

--- works/breath/0.0.1/1/alias_cd.sh
+++ works/breath/0.0.1/1/alias_cd.sh
@@ -1,9 +1,16 @@
+path=`dirname $0`
+shopt -s expand_aliases
 
-list1=` cat exports.cfg  | grep export | tr -s ' ' | cut -d' ' -f3 | cut -d'=' -f1`
-list2=` cat exports.cfg  | grep export | tr -s ' ' | cut -d' ' -f3 | cut -d'=' -f2`
+file=$path/alias_cd_dynamic.cfg
+echo -n '' > $file
+
+
+list1=` cat exports.cfg |grep dir_ | grep export | tr -s ' ' | cut -d' ' -f3 | cut -d'=' -f1 `
+
+#list2=` cat exports.cfg |grep dir_ | grep export | tr -s ' ' | cut -d' ' -f3 | cut -d'=' -f2`
 
 arr1=( $list1 )
-arr2=( $list2 )
+#arr2=( $list2 )
 
 max="${#arr1[@]}"
 
@@ -12,10 +19,18 @@ echo "After @ quoted expansion: ${#arr1[*]}"
 for ix in ${!arr1[*]}
 do
 
-        item="${arr1[$ix]}"
-#        printf "cd%s\n" "$item"
-item_content=`eval echo "\$$item"`
-echo -n "$item :"
-echo "$item_content"
 
-    done
+
+    item="${arr1[$ix]}"
+col2=`echo "$item" `
+col1=`echo "$item" | sed 's/dir_//g'`
+
+    #        printf "cd%s\n" "$item"
+    #item_content=`eval echo "\$$item"`
+    #echo -n "$item :"
+    echo "alias cd_$col1=\"cd \$$col2\"" >> $file
+
+done
+echo $file
+cat $file
+source $file
