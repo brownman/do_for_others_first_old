56692dd4040cd5ede88d25b33bcc4ffd6c1f1246
brownman <brownman556@gmail.com>
2014-03-12 15:52:32 +0000

    travis: +fix

--- works/test/0.0.1/test.sh
+++ works/test/0.0.1/test.sh
@@ -2,19 +2,20 @@
 #dir=package/do-for-others-first-0.0.1/usr/local/src/do-for-others-first/
 #wrapper=$dir/breath.sh
 ############################################  Directory structure:  ######################\
-export     runner_name=shunit
+    export     runner_name='shunit'
+echo "runner is $runner_name"
 echo "dir_root:$dir_root"
-    set -o nounset
-    pushd `dirname $0`>/dev/null
-    path=`pwd`
-    reset
+set -o nounset
+pushd `dirname $0`>/dev/null
+path=`pwd`
+reset
 dir_tests="$path/tests"
 dir_vendor="$dir_root/vendor"
 trigger_runner(){
     if [ "$runner_name" = shunit2 ];then
-    print_func
-    #trigger_runner
-    echo "[RUNNER] $runner"
+        print_func
+        #trigger_runner
+        echo "[RUNNER] $runner"
 
         echo "$runner"
         sleep 5
@@ -24,32 +25,32 @@ trigger_runner(){
 
 step0(){
     print_func
- export   dir_target=$path/tests
+    export   dir_target=$path/tests
     export -f trigger_runner
 }
 
 step1(){
     print_func
-    echo 'decide which runner'
+
     if [ "$runner_name" = shunit ];then
 
-    export runner=$dir_vendor/0.0.1/shunit2/src/shell/shunit2
+        export runner=$dir_vendor/0.0.1/shunit2/src/shell/shunit2
     else
-    export dir_runner=$dir_vendor/bash_koans
-    export runner=$dir_runner/runner_all.sh
+        export dir_runner=$dir_vendor/bash_koans
+        export runner=$dir_runner/runner_all.sh
 
     fi
 
 
     [ ! -f "$runner" ] && { echo 'invalid runner' "$runner";exit 1; }
-    export     runner_name=`basename $runner`
+    #    export     runner_name=`basename $runner`
 
-    cmd="vi $dir_runner" 
+    #    cmd="vi $dir_runner" 
 
-    echo  "[CLIPBOARD] $cmd" 
-    echo "$cmd" | /usr/bin/xsel --clipboard
+    #    echo  "[CLIPBOARD] $cmd" 
+    #    echo "$cmd" | /usr/bin/xsel --clipboard
 
-    sleep 3
+    #    sleep 3
 }
 
 step3(){
@@ -66,7 +67,7 @@ steps(){
     print_func
     step0
     step1
-#    step2
+    #    step2
     step3
 }
 
