879fa6a5b3731f7785001a5950f8c73616c6531c
brownman <brownman556@gmail.com>
2014-04-12 14:38:57 +0000

    breakthrough: use pv presentation inside the main layout

--- CONTROL/cron_recent_place.sh
+++ CONTROL/cron_recent_place.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-export dir_root=~/JAIL/TREE
-#source $dir_root/.bashrc_additions
 exec 2>/tmp/err
+export dir_root=~/JAIL/TREE
+source $dir_root/.trick.cfg
+
+proxy(){
+local    args=( $@ )
+local     cmd="${args[@]}"
+    echo "$cmd" >> /tmp/proxy
+    eval "$cmd"
+}
 trap_err(){
     notify-send error $?
 gxmessage -file /tmp/err -title trap_err -entry
@@ -9,22 +16,26 @@ gxmessage -file /tmp/err -title trap_err -entry
 trap trap_err ERR
 export -f trap_err
 #set -o nounset
-( flite -t 'there is only 1 true thing to do now - and you know it really really good' )&
+every 5 "flite -t 'there is only 1 true thing to do now - what is it'"
 #every 2 "$cmd"
 recent_place(){
-(    flite -t "I choosed to do for others first" &) # must not use proxy!
+
+every 5 " flite -t 'I choosed to do for others first'  # must not use proxy!"
 #cmd="gxmessage -entry -file /tmp/plan -title breakthrough -timout $timeout"
 #local str="$time:    $( \"$cmd\" )"
 #file_update "$file" "$str"
 
 local file_places="$dir_root/LIST/recent_places.txt"
-local str=$( cat $file_places | head -1 )
+local str=$( cat $file_places | tail -1 )
 local cmd="gxmessage -timeout 5 -entrytext \"$str\" -title 'recent_places' -file \"$file_places\" -sticky"
+
 local cmd1=$( eval  "$cmd" )
 if [ -n "$cmd1" ];then
-( proxy "$cmd1" )&
+    echo "$cmd1" >> $file_places
+ proxy "$cmd1" 
 else
-    ( proxy  "gvim $file_places" )&
+    #(killall -9 gvim 2>/dev/null &)
+ proxy   "gvim $file_places" 
     fi
 
 }
---
+++ VIEW/BANK/TOOLS/BANK/save_the_old/add_snipet_through_gui.sh
@@ -0,0 +1,30 @@
+reset
+history1(){
+grep "$1" -lr --exclude-dir='.git' "$dir_root" | grep .cfg
+}
+test1(){
+#info: translate
+#pending: add content
+echo
+}
+
+typy1(){
+    type  "$1"
+
+}
+
+steps(){
+    echo 'which keyword will help you save the script ?'
+    read input
+echo "input: $input"
+echo "-=-=-=-=-="
+echo type1 "$input"
+echo history1 "$input"
+echo test1 "$input"
+}
+
+
+
+
+
+steps
