5835b9b89975b9cd1266fa7622474bb3be6b261a
brownman <brownman556@gmail.com>
2014-04-14 15:20:14 +0000

    readme.md  - restore from old

---
+++ .test.sh
@@ -0,0 +1,5 @@
+source .trick.cfg
+while :;do
+ascii_art
+proxy sleep 10
+done
---
+++ WORKSPACE/BANK/reciepts/commands.sh
@@ -0,0 +1,2 @@
+exo-open http://www.baking.co.il/frena_moroccan_bread
+exo-open http://www.baking.co.il/frena_moroccan_bread
