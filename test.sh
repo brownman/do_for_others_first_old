d0fb92a0b34e67f5c00e26739ffaa525e23c5350
brownman <brownman556@gmail.com>
2014-04-16 20:29:55 +0000

    travis - loop over step5 - print only the ascii art

--- test.sh
+++ test.sh
@@ -14,15 +14,19 @@ ls /usr/bin
 }
 
 step5(){
-   wget -qO - http://www.asciiartfarts.com/random.cgi | sed -n '/<pre>/,/<\/pre>/p' | sed -n '/<table*/,/<\/table>/p' | sed '1d' | sed '$d' | recode html..ascii;
+   wget -qO - http://www.asciiartfarts.com/random.cgi | sed -n '/<pre>/,/<\/pre>/p' | sed -n '/<table*/,/<\/table>/p' | sed '1d' | sed '$d'
+   #| recode html..ascii;
 }
 
 
 steps(){
 #step1
-step3
-step4
+#step3
+#step4
+while 1:;do
 step5
+sleep 10
+done
 }
 
 steps
