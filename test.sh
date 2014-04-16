09be302e250f73892e906e3481eb476e6b484cba
brownman <brownman556@gmail.com>
2014-04-16 20:18:05 +0000

    travis - check env

--- test.sh
+++ test.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 set -o nounset
-export dir_root=`dirname $0`
-source $dir_root/.0_effort.cfg
-
 step1(){
+dir_root=`dirname $0`
+source $dir_root/.0_effort.cfg
 $dir_root/LAB/breath.cfg
 }
+step3(){
+env
+}
+step4(){
+ls /usr/bin
+}
 
-step2(){
+step5(){
    wget -qO - http://www.asciiartfarts.com/random.cgi | sed -n '/<pre>/,/<\/pre>/p' | sed -n '/<table*/,/<\/table>/p' | sed '1d' | sed '$d' | recode html..ascii;
 }
 
 
 steps(){
-step1
-step2
+#step1
+step3
+step4
+step5
 }
 
 steps
