9d1a7f85b3dac08e4ef623ef03208a495bdccd3f
brownman <brownman556@gmail.com>
2014-04-16 19:39:02 +0000

    travis.sh - add random ascii_art

--- .test.sh
+++ .test.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
 set -o nounset
+step1(){
 $dir_root/LAB/breath.cfg
+}
 
+step2(){
+   wget -qO - http://www.asciiartfarts.com/random.cgi | sed -n '/<pre>/,/<\/pre>/p' | sed -n '/<table*/,/<\/table>/p' | sed '1d' | sed '$d' | recode html..ascii;
+}
+
+
+steps(){
+step1
+step2
+}
+
+steps
